567e13a3b9543607ad1aff0cd5cf7b602cf3c11b
Hoi Sen <hoisenwong@outlook.com>
2023-03-06 00:53:27 -0500

    update

--- wk5hw/Assets/Scripts/EnemyController.cs
+++ wk5hw/Assets/Scripts/EnemyController.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
-    private GameObject player;
+    private Transform player;
 
-    private float speed = .1f;
+    private float speed = 1f;
+
+    private int MaxDist = 5;
+
+    private int MinDist = 2;
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectWithTag("Player");
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //this.transform.position = player.transform.position * speed;
+        //why enemy cannot follow the player - level1?
+        transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
     }
 }
